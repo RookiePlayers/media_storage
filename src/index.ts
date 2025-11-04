d2a11ffb03bade8cf8b743041ff9f5c5745c73c6
ollie2224 <olamidepeters@gmail.com>
2025-11-04 12:23:30 +0000

    fix: Update package-lock.json name and version; refactor index.ts to export services

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,7 @@
-import { IStorageService } from "./iStorage";
-import EnvironmentRegister from "./register";
-import { EnvironmentConfig, UploadParams } from "./types";
-
-/**
- * Thin wrapper binding a storage service to a config snapshot.
- */
-export class MediaStorage {
-  private mediaStorage: IStorageService;
-
-  constructor({
-    config,
-    service,
-    hydrateFromEnv = false,
-  }: {
-    config?: Partial<EnvironmentConfig>;
-    service: IStorageService;
-    hydrateFromEnv?: boolean;
-  }) {
-    const env = EnvironmentRegister.getInstance();
-    if (hydrateFromEnv) env.loadFromProcessEnv();
-    if (config) env.registerEnvironments(config);
-    this.mediaStorage = service;
-  }
-
-  public getStorageService(): IStorageService {
-    return this.mediaStorage;
-  }
-
-  public uploadFile(params: UploadParams) {
-    return this.mediaStorage.uploadFile(params);
-  }
-}
+export * from "./mediaStorage";
+export * from "./services/cloudFlareR2Storage";
+export * from "./services/firebaseStorage";
+export * from "./services/googleDriveStorage";
+export * from "./iStorage";
+export * from './register';
+export * from "./types";
